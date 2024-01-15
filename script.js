b292d322e56f333039be0696664bfc483f0392ca
Andrew Fennell <afennell@hotmail.co.uk>
2024-01-15 16:21:33 +0000

    Fixed responsivity issues

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ burgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("hidden");
   menuItems.classList.toggle("hidden");
 });
+
+function toggleReadMore(contentId, hiddenContentId, mobileContentId) {
+  const content = document.getElementById(contentId);
+  const hiddenContent = document.getElementById(hiddenContentId);
+  const mobileContent = document.getElementById(mobileContentId);
+
+  // Toggle the visibility of hidden content
+  hiddenContent.classList.toggle("hidden");
+
+  // Toggle the visibility of the "Read More" button on smaller screens
+  mobileContent.nextElementSibling.style.display =
+    mobileContent.nextElementSibling.style.display === "none"
+      ? "inline"
+      : "none";
+}
